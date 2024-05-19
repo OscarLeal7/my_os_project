aad4bef528ec437c76134a38ec50152ada785c2c
GanFar <133233370+GanFar@users.noreply.github.com>
2024-05-18 23:30:03 -0300

    Config Led

---
+++ src/drivers/led_driver.cpp
@@ -0,0 +1,19 @@
+// led_driver.cpp
+
+#include "led_driver.h"
+
+void init_led() {
+    // Inicialize o LED, por exemplo, configurando o pino GPIO correspondente
+}
+
+void turn_on_led() {
+    // Ligue o LED, por exemplo, definindo o pino GPIO como alto
+}
+
+void turn_off_led() {
+    // Desligue o LED, por exemplo, definindo o pino GPIO como baixo
+}
+
+void toggle_led() {
+    // Alterne o estado do LED, por exemplo, invertendo o estado do pino GPIO
+}
---
+++ src/drivers/led_driver.h
@@ -0,0 +1,11 @@
+// led_driver.h
+
+#ifndef LED_DRIVER_H
+#define LED_DRIVER_H
+
+void init_led();
+void turn_on_led();
+void turn_off_led();
+void toggle_led();
+
+#endif
