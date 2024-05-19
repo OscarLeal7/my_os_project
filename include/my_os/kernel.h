c2b036c50a8d9b01900e4d46e0c732563cc58ad6
GanFar <133233370+GanFar@users.noreply.github.com>
2024-05-18 23:15:32 -0300

    Config Basic Kernel

--- include/my_os/kernel.h
+++ include/my_os/kernel.h
@@ -1,1 +1,10 @@
 // Kernel header
+// Descrição: Cabeçalho do kernel
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+// Função de inicialização do kernel
+void kernel_init();
+
+#endif // KERNEL_H
--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,6 +1,14 @@
 #include "../include/my_os/kernel.h"
 
+// Função principal
 int main() {
-    // Kernel entry point
+    // Chama a função de inicialização do kernel
+    kernel_init();
+
+    // Loop infinito
+    while (true) {
+        // Coloque aqui o código que será executado pelo kernel
+    }
+
     return 0;
 }
